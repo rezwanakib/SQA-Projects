2458fad92f791fd17a328e32ae662ec2714d68d7
rezwanakib <rezwanakib@gmail.com>
2024-06-29 00:38:42 +0600

    commit

---
+++ Unit2FinalTask/Selenium/BaseTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+
+namespace Unit2FinalTask
+{
+    internal class BaseTest
+    {
+        protected WebDriver driver;
+        protected static readonly string url = "https://www.wikipedia.org/";
+        protected static readonly string preciseTextXpath = "//*[text()='{0}']";
+        protected static readonly string relativePathFolder = @"Resources\"; 
+        protected WebDriverWait wait = null;
+        protected static readonly int maxWait = 10;
+
+        [SetUp]
+        public void Setup()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddUserProfilePreference("download.default_directory", Path.GetFullPath(relativePathFolder));
+
+            driver = new ChromeDriver(chromeOptions);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(maxWait));
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ Unit2FinalTask/Selenium/Unit2FinalTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using OpenQA.Selenium;
+using System.IO;
+
+namespace Unit2FinalTask.Selenium
+{
+    internal class Unit2FinalTest : BaseTest
+    {
+        private static readonly string searchValue = "Albert Einstein";
+        private static readonly string pageTitle = "Wikipedia";
+
+        private static readonly By searchField = By.XPath("//*[@id='searchInput']");
+        private static readonly By submitBtn = By.XPath("//*[@class='pure-button pure-button-primary-progressive']");
+        private static readonly By toolBtn = By.XPath("//*[@id='vector-page-tools-dropdown-checkbox']");
+        //private static readonly By downloadAsPDF = By.XPath("//*[@id='coll-download-as-rl']/a");
+        private static readonly By downloadAsPDF = By.XPath("//*[@id='coll-download-as-rl']//a[contains(@href,'download')]");
+        private static readonly By fileDownloadBtn = By.XPath(string.Format(preciseTextXpath, "Download"));
+        
+        private FileInfo downloadedFile;
+
+        [Test]
+        public void FinalTest()
+        {
+            ClassicAssert.AreEqual(pageTitle, driver.Title, "Main page is not displayed.");
+
+            var searchFieldElement = driver.FindElement(searchField);
+            searchFieldElement.SendKeys(searchValue);
+
+            driver.FindElement(submitBtn).Click();
+            driver.FindElement(toolBtn).Click();
+            driver.FindElement(downloadAsPDF).Click();
+
+            By classname = By.ClassName("mw-electronpdfservice-selection-label-desc");
+            string fileName = driver.FindElement(classname).Text;
+
+            driver.FindElement(fileDownloadBtn).Click();
+
+            By fileNameField = By.XPath(string.Format(preciseTextXpath, fileName));
+
+            driver.FindElement(fileNameField).Click();
+
+            string filePath = relativePathFolder + fileName;
+            downloadedFile = new FileInfo(Path.GetFullPath(filePath));
+
+            wait.Until(condition => IsFileDownloaded(filePath));
+            Assert.That(IsFileDownloaded(filePath), "File not downloaded");
+        }
+
+        private bool IsFileDownloaded(string filePath)
+        {
+            var newFile = new FileInfo(Path.GetFullPath(filePath));
+            return newFile.Exists;
+        }
+
+        [TearDown]
+        public void DeleteFile()
+        {
+            if (downloadedFile.Exists)
+                downloadedFile.Delete();
+        }
+    }
+}
